f23fdca5a272bdd6ae3dfba7d13073370894fbcb
fayannie <zw19870201@hotmail.com>
2012-03-21 12:13:43 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdaptImages::Application.config.secret_token = '9afa4270fdf59d027de812234a9093f05faa337731ceae1124918c336f3c8da9c8e64067fbfb31ed1b94e57da4a5977974d41bf88effb83192403a305dac3eaa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AdaptImages::Application.config.session_store :cookie_store, :key => '_AdaptImages_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AdaptImages::Application.config.session_store :active_record_store
